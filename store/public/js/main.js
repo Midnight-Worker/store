6e03295a0a3e394a6486527f42ee8585ce0eb4ed
Maik Tappe <maik.tappe82@googlemail.com>
2025-09-08 22:01:06 +0200

    bla

--- store/public/js/main.js
+++ store/public/js/main.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('klickMich');
-  btn.addEventListener('click', () => {
-    alert('Hallo vom Browser-JavaScript!');
-  });
 
-document.addEventListener('keydown', (e) => {
+  document.addEventListener('keydown', (e) => {
   // Ctrl+R oder F5
    if ((e.ctrlKey && e.key.toLowerCase() === 'r') || e.key === 'F5') {
      e.preventDefault();
@@ -19,6 +15,7 @@ document.addEventListener('keydown', (e) => {
   $(function(){
     $('input, textarea, [contenteditable=true]').virtualKeyboard();
   });
+
 
 });
 
